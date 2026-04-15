68378f324346ff9d2d49330631609418a5351fd4
Matthew Bledsoe <mbledso1@trinity.edu>
2026-04-15 16:48:21 -0500

    fixed CSS merge

--- server.js
+++ server.js
@@ -33,8 +33,7 @@ db = mysql.createConnection({
         host: process.env.DB_HOST,
         user: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME,
-        port: process.env.DB_PORT
+        database: process.env.DB_NAME
 });
 
 app.listen(port, () => {
